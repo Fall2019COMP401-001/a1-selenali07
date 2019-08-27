784ba99d13b104d1fe84d52aaa7f942f2b591b69
Selena <selenali@live.unc.edu>
2019-08-27 19:29:22 -0400

    idk

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,27 +3,16 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
-	
-	/* you shouldn't have to use any Scanner methods 
-	 * other than next(), nextInt(), and nextDouble()
-	 * - "KMP"
-	 */
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 
-
-		// Your code follows here.
-		
-		
 		int count = scan.nextInt();
 		
 		String[] customers = new String[count];
 		
-		
 		for(int i = 0; i < count; i++) {
-			
 			
 			char firstName = scan.next().charAt(0);
 			
@@ -45,7 +34,12 @@ public class A1Novice {
 			
 			}
 			
-			System.out.println(firstName + ". " + lastName + ": " + String.format("%.2f", cost));
+			customers[i] = firstName + ". " + lastName + ": " + String.format("%.2f", cost);
+		}
+		
+		for (int i=0; i<customers.length; i++) {
+			
+			System.out.println(customers[i]);
 			
 		}
 		scan.close();
