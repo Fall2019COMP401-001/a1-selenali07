301c87a2f30d0d14a5b3a990831dd70eb803bef2
Selena <selenali@live.unc.edu>
2019-08-27 19:43:55 -0400

    yay

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,30 +6,22 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
-		int count = scan.nextInt();
+		System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
+		Scanner scan = new Scanner(System.in);
+		int count = scan.nextInt();
 		String[] customers = new String[count];
 		
 		for(int i = 0; i < count; i++) {
-			
 			char firstName = scan.next().charAt(0);
-			
 			String lastName = scan.next();
-			
 			int items = scan.nextInt();
-			
 			double cost = 0;
 			
 			for(int j = 0; j < items; j++) {
-				
 				int amount = scan.nextInt();
-				
 				String product = scan.next();
-				
 				double price = scan.nextDouble();
-				
 				cost += amount * price;
 			
 			}
